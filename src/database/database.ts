b3990b0da7d181eca45038832e41640f6917f455
Jackson Dooley <jacksondooley@jacksons-mbp.lan>
2024-10-12 23:26:49 -0400

    add src dir

---
+++ src/database/database.ts
@@ -0,0 +1,8 @@
+import { drizzle } from 'drizzle-orm/better-sqlite3';
+import Database from 'better-sqlite3';
+import { users } from '../models/users';
+
+const sqlite = new Database('sqlite.db');
+export const db = drizzle(sqlite);
+
+export const schema = { users };
